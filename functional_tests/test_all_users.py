e87823cf26cc9807bb4d6444529189bb7fe449c9
Assela Pathirana <assela@pathirana.net>
2015-05-31 00:55:58 +0200

    Functional tests passing for static files including favicon.ico

--- functional_tests/test_all_users.py
+++ functional_tests/test_all_users.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from django.core.urlresolvers import reverse
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase  
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from unittest import skip
+import re
 
 
 class NewVisitorTest(StaticLiveServerTestCase):
@@ -16,21 +18,54 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def get_full_url(self, namespace):
         return self.live_server_url + reverse(namespace)    
 
-
+    
     def test_home_title(self):
         self.browser.get(self.get_full_url("home"))
         self.assertIn("TaskBuster", self.browser.title)
+        
     
     def test_h1_css(self):
             self.browser.get(self.get_full_url("home"))
             h1 = self.browser.find_element_by_tag_name("h1")
             self.assertEqual(h1.value_of_css_property("color"),
                              "rgba(200, 50, 255, 1)")
-            
+           
     def test_home_files(self):
-        response=self.client.get(self.live_server_url + "/robots.txt")
-        self.assertContains(response,"www.robotstxt.org")
+        self.browser.get(self.live_server_url + "/robots.txt")
         self.assertNotIn("Not Found", self.browser.title)
-        response=self.client.get(self.live_server_url + "/humans.txt")
-        self.assertContains(response,"humanstxt.org")
-        self.assertNotIn("Not Found", self.browser.title)    
+        self.assertEqual("",self.browser.title)
+        self.browser.get(self.live_server_url + "/humans.txt")
+        self.assertNotIn("Not Found", self.browser.title) 
+        self.assertEqual("",self.browser.title)
+    
+    def test_favicon(self):
+        self.browser.get(self.get_full_url("home"))
+        links=self.browser.find_elements_by_xpath("//head/link")
+        p=re.compile('.+favicon.ico$')
+        link=[x for x in links if p.match(x.get_attribute(name='href'))][0]
+        self.assertEqual(link.get_attribute(name='rel'),"shortcut icon")
+        a=link.get_attribute(name='href')
+        self.browser.get(a)
+        self.assertIn("favicon.ico",self.browser.title)
+        self.assertNotIn("Not Found", self.browser.title)         
+
+
+ 
+  
+# DO not use direct calling to run these. The main below is only for exploratory coding.   
+if __name__=="__main__":
+    print ("WARNING: This is not the way to run tests!! (use ./manage.py test ..)")
+    nw=NewVisitorTest()
+    nw.setUp()
+    nw.browser.get("http://localhost:8000/")
+    links=nw.browser.find_elements_by_xpath("//head/link")
+    p=re.compile('.+favicon.ico$')
+    link=[x for x in links if p.match(x.get_attribute(name='href'))][0]
+    nw.assertRegex(link.get_attribute(name='rel'),"shortcut icon")    
+    response=nw.client.get(link.get_attribute(name='href'))
+    nw.assertContains(response,None,status_code=200)    
+    nw.tearDown()
+    
+    
+
+        
