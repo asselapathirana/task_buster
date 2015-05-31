9b995352ce8ff216ee2bcdf0d37d5c86bb9a0023
Assela Pathirana <assela@pathirana.net>
2015-05-31 14:27:19 +0200

    Internationalization: Language support added. All tests pass

--- taskbuster/tests.py
+++ taskbuster/tests.py
@@ -7,16 +7,20 @@ from django.conf import settings
 from django.utils.translation import activate
 
 class TestHomePage(TestCase):
+    def _activate():
+        activate(settings.LANGUAGES[0][0])
+        
     def test_uses_index_template(self):
         for l in settings.LANGUAGES: 
             activate(l[0])
-            print ("Activating:",l[0])
             response = self.client.get(reverse("home"))
             self.assertTemplateUsed(response, "taskbuster/index.html")
         
     def test_uses_base_template(self):
-        response = self.client.get(reverse("home"))
-        self.assertTemplateUsed(response, "base.html")
+        for l in settings.LANGUAGES: 
+            activate(l[0])        
+            response = self.client.get(reverse("home"))
+            self.assertTemplateUsed(response, "base.html")
         
 class TestSettingsForTranslations(TestCase):
     
@@ -25,8 +29,12 @@ class TestSettingsForTranslations(TestCase):
         self.assertIn('django.middleware.locale.LocaleMiddleware',settings.MIDDLEWARE_CLASSES)
         self.assertGreaterEqual(len(settings.LANGUAGES),1)
         self.assertEqual(len(settings.LANGUAGES[0]),2)
-        self.assertRegex(settings.LANGUAGE_CODE,'.+-.+')
+        self.assertRegex(settings.LANGUAGE_CODE,'[a-z]+.*')
         for path in settings.LOCALE_PATHS:
             self.assertTrue(os.path.exists(path))
+            
+    def  test_LANGUAGE_CODE_in_LANGUAGES(self):
+        language_codes=[x[0] for x in settings.LANGUAGES]
+        self.assertIn(settings.LANGUAGE_CODE,language_codes)
             
         
